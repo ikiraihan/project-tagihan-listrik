a6706964da88ac2d84a543fb98cb21840a127c51
Muhammad Raihan <pradanaraihan963@gmail.com>
2023-09-27 22:46:06 +0700

    feat: add migration and model table pelanggan & tagihan

--- database/migrations/2023_09_25_103751_create_pelanggan_table.php
+++ database/migrations/2023_09_25_103751_create_pelanggan_table.php
@@ -11,14 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('pelanggan', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('username')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('id_pelanggan');
+            $table->string('nama');
+            $table->string('alamat');
+            $table->string('daya');
             $table->timestamps();
         });
     }
@@ -28,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('pelanggan');
     }
 };
--- database/migrations/2023_09_25_103806_create_tagihan_table.php
+++ database/migrations/2023_09_25_103806_create_tagihan_table.php
@@ -11,14 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('tagihan', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('username')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('id_pelanggan');
+            $table->string('bulan');
+            $table->string('tahun');
+            $table->string('daya');
+            $table->string('kelas_tarif');
+            $table->string('total_tagihan');
             $table->timestamps();
         });
     }
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('tagihan');
     }
 };
